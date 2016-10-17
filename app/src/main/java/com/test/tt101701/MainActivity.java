f34c757bf359270c349ab858f5012ffba42953a4
Shaojung <shaojung@gmail.com>
2016-10-17 10:28:18 +0800

    Finish db read

--- app/src/main/java/com/test/tt101701/MainActivity.java
+++ app/src/main/java/com/test/tt101701/MainActivity.java
@@ -1,8 +1,12 @@
 package com.test.tt101701;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,17 +15,30 @@ import java.io.OutputStream;
 public class MainActivity extends AppCompatActivity {
 
     final String DB_NAME = "address.db";
-    final String DB_PATH = "/data/data/" + getPackageName() + "/databases/";
+    final String DB_PATH = "/data/data/com.test.tt101701/databases/";
+    final String PACKAGE_NAME = "com.test.tt101701";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File dbFile = new File(DB_PATH + DB_NAME);
         try {
-            copyDataBase();
+            if (!dbFile.exists())
+            {
+                copyDataBase();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(dbFile, null);
+        Cursor c = db.rawQuery("Select * from phonebook", null);
+        c.moveToFirst();
+        Log.d("DATA", c.getString(1));
+        c.close();
+        db.close();
+
     }
 
     private void copyDataBase() throws IOException {
@@ -31,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Path to the just created empty db
         String outFileName = DB_PATH + DB_NAME;
+
+        File f = new File(DB_PATH);
+        if (!f.exists())
+        {
+            f.mkdir();
+        }
 
         //Open the empty db as the output stream
         OutputStream myOutput = new FileOutputStream(outFileName);
